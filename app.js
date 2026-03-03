d49be556c057f47b4c8616b4d06f22532d0f69f0
erikgonza03080 <erikgonza03080@gmail.com>
2026-03-02 23:53:20 -0600

    Pagina Terminada

---
+++ app.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const botones = document.querySelectorAll(".btn-ver-pdf");
+  const pdfViewer = document.getElementById("pdfViewer");
+  const tituloPdf = document.getElementById("tituloPdf");
+  const btnDescargarPdf = document.getElementById("btnDescargarPdf");
+
+  // Mapa de PDFs
+  const pdfs = {
+    informatica: "pdf/peritoInformatica.pdf",
+    administracion: "pdf/peritoAdministracion.pdf",
+    contador: "pdf/peritoContador.pdf",
+    electronica: "pdf/peritoElectronica.pdf",
+    mecanica: "pdf/peritoAutomotriz.pdf"
+  };
+
+  botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+      const carrera = boton.dataset.carrera;
+      const nombre = boton.dataset.nombre;
+      const rutaPdf = pdfs[carrera];
+      document.getElementById("visor-pdf").style.display='none';
+      if (!rutaPdf) {
+        tituloPdf.textContent = "PDF no disponible";
+        pdfViewer.src = "";
+        btnDescargarPdf.href = "#";
+        btnDescargarPdf.classList.add("disabled");
+        return;
+      }
+    document.getElementById("visor-pdf").style.display='block';
+      // Cambiar título
+      tituloPdf.textContent = `Lista de útiles - ${nombre}`;
+
+      // Cargar vista previa
+      pdfViewer.src = rutaPdf;
+
+      // Activar botón de descarga
+      btnDescargarPdf.href = rutaPdf;
+      btnDescargarPdf.setAttribute("download", `${carrera}.pdf`);
+      btnDescargarPdf.classList.remove("disabled");
+
+      // Desplazar al visor
+      document.getElementById("visor-pdf").scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+
+    
+  });
+
+  // Smooth scroll para menú interno
+  const enlacesInternos = document.querySelectorAll('a[href^="#"]');
+
+  enlacesInternos.forEach((enlace) => {
+    enlace.addEventListener("click", function (e) {
+      const destino = document.querySelector(this.getAttribute("href"));
+
+      if (destino) {
+        e.preventDefault();
+        destino.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+});
